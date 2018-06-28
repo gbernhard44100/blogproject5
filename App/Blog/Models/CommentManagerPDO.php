9da91bd227ff38aff932db42efa80051c1c34edf
gbernhard44100 <34214329+gbernhard44100@users.noreply.github.com>
2018-06-28 14:55:01 +0200

    PSR-2 reached

--- App/Blog/Models/CommentManagerPDO.php
+++ App/Blog/Models/CommentManagerPDO.php
@@ -5,9 +5,11 @@ namespace App\Blog\Models;
 use Lib\GBFram\ManagerPDO;
 use Lib\GBFram\Application;
 
-class CommentManagerPDO extends ManagerPDO {
+class CommentManagerPDO extends ManagerPDO 
+{
 
-    function __construct(Application $app, $dao) {
+    function __construct(Application $app, $dao) 
+    {
         parent::__construct($app, $dao);
         $this->table = 'comment';
     }
