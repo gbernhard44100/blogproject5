ecfabbe7f9cc7888543531fc3eca6682591b4260
gbernhard44100 <34214329+gbernhard44100@users.noreply.github.com>
2018-06-28 15:44:18 +0200

    PSR-2 reached

--- Lib/GBFram/Form/TextArea.php
+++ Lib/GBFram/Form/TextArea.php
@@ -2,29 +2,35 @@
 
 namespace Lib\GBFram\Form;
 
-class TextArea extends Field {
-
-    function __construct(array $data) {
-        parent::__construct($data);
-        $this->fieldType = 'TextArea';
-    }
+class TextArea extends Field 
+{
 
     Private $_rows = "10";
     Private $_cols = "80";
 
-    public function rows() {
+    function __construct(array $data) 
+    {
+        parent::__construct($data);
+        $this->fieldType = 'TextArea';
+    }
+
+    public function rows() 
+    {
         return $this->_rows;
     }
 
-    public function cols() {
+    public function cols() 
+    {
         return $this->_cols;
     }
 
-    public function setRows(int $rows) {
+    public function setRows(int $rows) 
+    {
         $this->_rows = $rows;
     }
 
-    public function setCols(int $cols) {
+    public function setCols(int $cols) 
+    {
         $this->_cols = $cols;
     }
 
