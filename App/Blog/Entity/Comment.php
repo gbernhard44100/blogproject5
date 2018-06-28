c17cb09ea656fe592802419b1686aa86e01d3cf6
gbernhard44100 <34214329+gbernhard44100@users.noreply.github.com>
2018-06-28 14:49:04 +0200

    PSR-2 reached

--- App/Blog/Entity/Comment.php
+++ App/Blog/Entity/Comment.php
@@ -4,7 +4,8 @@ namespace App\Blog\Entity;
 
 use \Lib\GBFram\Entity;
 
-class Comment extends Entity {
+class Comment extends Entity 
+{
 
     protected $id;
     protected $author;
@@ -13,53 +14,65 @@ class Comment extends Entity {
     protected $updateDate;
     protected $idBlog;
 
-    public function id() {
+    public function id() 
+    {
         return $this->id;
     }
 
-    public function author() {
+    public function author() 
+    {
         return $this->author;
     }
 
-    public function content() {
+    public function content() 
+    {
         return $this->content;
     }
 
-    public function valid() {
+    public function valid() 
+    {
         return $this->valid;
     }
 
-    public function updateDate() {
+    public function updateDate() 
+    {
         return $this->updateDate;
     }
 
-    public function idBlog() {
+    public function idBlog() 
+    {
         return $this->idBlog;
     }
 
-    public function setId(int $id) {
+    public function setId(int $id)
+    {
         if ($id > 0) {
             $this->id = $id;
         }
     }
 
-    public function setValid() {
+    public function setValid() 
+    {
         $this->valid = TRUE;
     }
 
-    public function setAuthor(string $author) {
+    public function setAuthor(string $author) 
+    {
         $this->author = $author;
     }
 
-    public function setContent(string $content) {
+    public function setContent(string $content) 
+    {
         $this->content = $content;
     }
 
-    public function setUpdateDate($updateDate) {
+    public function setUpdateDate($updateDate) 
+    {
         $this->updateDate = $updateDate;
     }
 
-    public function setIdBlog(int $id) {
+    public function setIdBlog(int $id) 
+    {
         if ($id > 0) {
             $this->idBlog = $id;
         }
