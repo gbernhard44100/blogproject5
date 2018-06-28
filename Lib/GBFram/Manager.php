8d9ac1fc15f37fe035b2517679878b32c1c9c7b0
gbernhard44100 <34214329+gbernhard44100@users.noreply.github.com>
2018-06-28 15:27:49 +0200

    PSR-2 reached

--- Lib/GBFram/Manager.php
+++ Lib/GBFram/Manager.php
@@ -2,16 +2,19 @@
 
 namespace Lib\GBFram;
 
-abstract class Manager extends ApplicationComponent {
+abstract class Manager extends ApplicationComponent 
+{
 
     protected $dao;
 
-    public function __construct(Application $app, $dao) {
+    public function __construct(Application $app, $dao) 
+    {
         parent::__construct($app);
         $this->dao = $dao;
     }
 
-    public function dao() {
+    public function dao() 
+    {
         return $this->dao;
     }
 
