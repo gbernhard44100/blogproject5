8cea3616a2208542fa7e7dfdbe99705506cf824f
gbernhard44100 <34214329+gbernhard44100@users.noreply.github.com>
2018-06-28 15:46:59 +0200

    PSR-2 reached

--- Lib/GBFram/Form/Input.php
+++ Lib/GBFram/Form/Input.php
@@ -2,20 +2,24 @@
 
 namespace Lib\GBFram\Form;
 
-class Input extends Field {
+class Input extends Field 
+{
 
     private $_type = 'text';
 
-    function __construct(array $data) {
+    function __construct(array $data) 
+    {
         parent::__construct($data);
         $this->fieldType = 'Input';
     }
 
-    public function type() {
+    public function type() 
+    {
         return $this->_type;
     }
 
-    public function setType($type) {
+    public function setType($type) 
+    {
         $alltypes = array('button', 'checkbox', 'color', 'date', 'datetime-local',
             'email', 'file', 'hidden', 'image', 'month', 'number', 'password',
             'radio', 'range', 'reset', 'search', 'submit', 'tel', 'text', 'time',
