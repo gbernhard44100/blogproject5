d337f79bc4d25cbbcda9237ddff4579d75740c70
gbernhard44100 <34214329+gbernhard44100@users.noreply.github.com>
2018-06-28 15:31:37 +0200

    PSR-2 reached

--- Lib/GBFram/HTTPRequest.php
+++ Lib/GBFram/HTTPRequest.php
@@ -2,48 +2,59 @@
 
 namespace Lib\GBFram;
 
-class HTTPRequest extends ApplicationComponent {
+class HTTPRequest extends ApplicationComponent 
+{
 
     protected $route;
 
-    public function route() {
+    public function route() 
+    {
         return $this->route;
     }
 
-    public function setRoute($route) {
+    public function setRoute($route) 
+    {
         $this->route = $route;
         $_GET = array_merge($_GET, $this->route->vars());
     }
 
-    public function cookieData($key) {
+    public function cookieData($key) 
+    {
         return isset($_COOKIE[$key]) ? $_COOKIE[$key] : null;
     }
 
-    public function cookieExists($key) {
+    public function cookieExists($key) 
+    {
         return isset($_COOKIE[$key]);
     }
 
-    public function getData($key) {
+    public function getData($key) 
+    {
         return isset($_GET[$key]) ? $_GET[$key] : null;
     }
 
-    public function getExists($key) {
+    public function getExists($key) 
+    {
         return isset($_GET[$key]);
     }
 
-    public function method() {
+    public function method() 
+    {
         return $_SERVER['REQUEST_METHOD'];
     }
 
-    public function postData($key) {
+    public function postData($key) 
+    {
         return isset($_POST[$key]) ? $_POST[$key] : null;
     }
 
-    public function postExists($key) {
+    public function postExists($key) 
+    {
         return isset($_POST[$key]);
     }
 
-    public function requestURI() {
+    public function requestURI() 
+    {
         return $_SERVER['REQUEST_URI'];
     }
 
