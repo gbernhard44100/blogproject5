dbf5fe5ba31582c1b1b7c9dac95e35d74affb2cc
gbernhard44100 <34214329+gbernhard44100@users.noreply.github.com>
2018-06-28 15:23:03 +0200

    PSR-2 reached

--- Lib/GBFram/Managers.php
+++ Lib/GBFram/Managers.php
@@ -2,29 +2,30 @@
 
 namespace Lib\GBFram;
 
-class Managers extends ApplicationComponent {
+class Managers extends ApplicationComponent 
+{
 
     protected $api = null;
     protected $dao = null;
     protected $managers = [];
 
-    public function __construct($app, $api, $dao) {
+    public function __construct($app, $api, $dao) 
+    {
         parent::__construct($app);
 
         $this->api = $api;
         $this->dao = $dao;
     }
 
-    public function getManagerOf($module) {
+    public function getManagerOf($module) 
+    {
         if (!is_string($module) || empty($module)) {
             throw new \InvalidArgumentException('Le module spécifié est invalide');
         }
-
         if (!isset($this->managers[$module])) {
             $manager = 'App\\' . $this->app->name() . '\\Models\\' . $module . 'Manager' . $this->api;
             $this->managers[$module] = new $manager(parent::app(), $this->dao);
         }
-
         return $this->managers[$module];
     }
 
