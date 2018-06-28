95ec786e4b6907cac14ae00fa43476a3c7811024
gbernhard44100 <34214329+gbernhard44100@users.noreply.github.com>
2018-06-28 15:21:41 +0200

    PSR-2 reached

--- Lib/GBFram/Page.php
+++ Lib/GBFram/Page.php
@@ -5,26 +5,26 @@ namespace Lib\GBFram;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
-class Page extends ApplicationComponent {
+class Page extends ApplicationComponent 
+{
 
     protected $contentFile;
     protected $vars = [];
     protected $twig;
-    protected $twig_templates_path = array();
+    protected $twigTemplatesPath = array();
 
-    function __construct(Application $app) {
+    function __construct(Application $app) 
+    {
         parent::__construct($app);
 
         $xml = new \DOMDocument;
         $xml->load(__DIR__ . '/../../App/' . $this->app->name() . '/Config/twig.xml');
-
         $elements = $xml->getElementsByTagName('define');
 
         foreach ($elements as $element) {
-            $this->twig_templates_path[] = $element->getAttribute('path');
+            $this->twigTemplatesPath[] = $element->getAttribute('path');
         }
-
-        $templatesloader = new FilesystemLoader($this->twig_templates_path);
+        $templatesloader = new FilesystemLoader($this->twigTemplatesPath);
         $this->twig = new Environment($templatesloader, array(
             'cache' => FALSE,
             'debug' => true,
@@ -34,29 +34,30 @@ class Page extends ApplicationComponent {
         $this->twig->addGlobal('session', $_SESSION);
     }
 
-    public function addVar($var, $value) {
+    public function addVar($var, $value) 
+    {
         if (!is_string($var) || is_numeric($var) || empty($var)) {
             throw new \InvalidArgumentException('Le nom de la variable doit être une chaine de caractères non nulle');
         }
-
         $this->vars[$var] = $value;
     }
 
-    public function getGeneratedPage() {
+    public function getGeneratedPage() 
+    {
         $fileExist = False;
-        foreach ($this->twig_templates_path as $path) {
+        foreach ($this->twigTemplatesPath as $path) {
             if (file_exists($path . '/' . $this->contentFile)) {
                 $fileExist = TRUE;
             }
         }
-
         if (!isset($fileExist)) {
             throw new \RuntimeException('La vue spécifiée n\'existe pas');
         }
         $this->twig->display($this->contentFile, $this->vars);
     }
 
-    public function setContentFile($contentFile) {
+    public function setContentFile($contentFile) 
+    {
         if (!is_string($contentFile) || empty($contentFile)) {
             throw new \InvalidArgumentException('La vue spécifiée est invalide');
         }
